4d19f0821d44f830f1f604ec412ac094a95cfbe0
Alasdair McLeay <alasdair.mcleay2@ibm.com>
2018-11-22 15:05:07 +0000

    trim govuk from start of module names

--- lib/convertScopeToModuleName.ts
+++ lib/convertScopeToModuleName.ts
@@ -6,5 +6,6 @@ export function convertScopeToModuleName(scope: string) {
         .replace(
             /^(break|case|catch|continue|debugger|default|delete|do|else|finally|for|function|if|in|instanceof|new|return|switch|this|throw|try|typeof|var|void|while|with)$/,
             "_$1",
-        );
+        )
+        .replace(/^govuk/, "");
 }
